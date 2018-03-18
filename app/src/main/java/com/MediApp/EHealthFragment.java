e4b2af0ab8dbe974cd452ebf4fce01a14a0886ab
honeycoder96 <softwaretester2016pro@gmail.com>
2018-03-18 05:57:56 +0530

    commiting changes

--- app/src/main/java/com/MediApp/EHealthFragment.java
+++ app/src/main/java/com/MediApp/EHealthFragment.java
@@ -19,122 +19,19 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class EHealthFragment extends Fragment {
 
-    private FirebaseRecyclerAdapter firebaseRecyclerAdapter;
-    private DatabaseReference mDatabase;
-
-    private RecyclerView mHospitalList;
-
-    View mMainView;
+    public View mMainView;
     public EHealthFragment() {
         // Required empty public constructor
     }
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mDatabase= FirebaseDatabase.getInstance().getReference().child("hospital");
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mMainView=inflater.inflate(R.layout.fragment_ehealth, container, false);
-
-
-        mHospitalList =(RecyclerView)mMainView.findViewById(R.id.hospital_list);
-        mHospitalList.setHasFixedSize(true);
-        mHospitalList.setLayoutManager(new LinearLayoutManager(getContext()));
-        firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Hospital, HospitalViewHolder>
-                (Hospital.class, R.layout.hospitals_row, HospitalViewHolder.class,
-                        mDatabase) {
-
-
-            @Override
-            protected void populateViewHolder(HospitalViewHolder viewHolder, Hospital model, int position) {
-
-                final String post_key = getRef(position).getKey();
-
-                viewHolder.setName(model.getName());
-                viewHolder.setDesc(model.getDescription());
-
-                viewHolder.mview.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Toast.makeText(getContext(), "Getting Hospital Details", Toast.LENGTH_LONG).show();
-
-                    }
-                });
-            }
-
-        };
-
-        mHospitalList.setAdapter(firebaseRecyclerAdapter);
     return mMainView;
     }
 
-
-
-
-
-    public static class HospitalViewHolder extends RecyclerView.ViewHolder{
-
-        View mview;
-        TextView hospital_name,hospital_dese;
-
-        ImageView map,contact,website;
-
-        FirebaseAuth mAuth;
-
-        public HospitalViewHolder(View itemView) {
-            super(itemView);
-            mview=itemView;
-
-
-            mAuth=FirebaseAuth.getInstance();
-            hospital_name=(TextView)mview.findViewById(R.id.hospitalname);
-
-            hospital_dese= (TextView) mview.findViewById(R.id.hospitaldesc);
-
-            map=(ImageView)mview.findViewById(R.id.mapimg);
-            contact=(ImageView)mview.findViewById(R.id.phoneimg);
-            website=(ImageView)mview.findViewById(R.id.websiteimg);
-
-            map.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                }
-            });
-
-            contact.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                }
-            });
-
-            website.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-
-                }
-            });
-
-        }
-
-        public void setName(String name){
-            TextView hopitalname=(TextView)mview.findViewById(R.id.hospitalname);
-            hopitalname.setText(name);
-        }
-
-
-        public void setDesc(String desc){
-            TextView hospital_desc=(TextView)mview.findViewById(R.id.hospitaldesc);
-            hospital_desc.setText(desc);
-        }
-
-    }
 
 }
--- app/src/main/java/com/MediApp/Hospital.java
+++ app/src/main/java/com/MediApp/Hospital.java
@@ -5,70 +5,60 @@ package com.MediApp;
  */
 
 public class Hospital {
-    private String name;
-    private String description;
-    private String latitude;
-    private String longitude;
-    private String contact;
-    private String website;
+    private String Hospital_Name;
+    private String Location_Coordinates;
+    private String Location;
+    private String Helpline;
+    private String Website;
 
     public Hospital() {
     }
 
-    public Hospital(String name, String description, String latitude, String longitude, String contact, String website) {
-        this.name = name;
-        this.description = description;
-        this.latitude = latitude;
-        this.longitude = longitude;
-        this.contact = contact;
-        this.website = website;
+    public Hospital(String hospital_Name, String location_Coordinates, String location, String helpline, String website) {
+        Hospital_Name = hospital_Name;
+        Location_Coordinates = location_Coordinates;
+        Location = location;
+        Helpline = helpline;
+        Website = website;
     }
 
-    public String getName() {
-        return name;
+    public String getHospital_Name() {
+        return Hospital_Name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setHospital_Name(String hospital_Name) {
+        Hospital_Name = hospital_Name;
     }
 
-    public String getDescription() {
-        return description;
+    public String getLocation_Coordinates() {
+        return Location_Coordinates;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setLocation_Coordinates(String location_Coordinates) {
+        Location_Coordinates = location_Coordinates;
     }
 
-    public String getLatitude() {
-        return latitude;
+    public String getLocation() {
+        return Location;
     }
 
-    public void setLatitude(String latitude) {
-        this.latitude = latitude;
+    public void setLocation(String location) {
+        Location = location;
     }
 
-    public String getLongitude() {
-        return longitude;
+    public String getHelpline() {
+        return Helpline;
     }
 
-    public void setLongitude(String longitude) {
-        this.longitude = longitude;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
+    public void setHelpline(String helpline) {
+        Helpline = helpline;
     }
 
     public String getWebsite() {
-        return website;
+        return Website;
     }
 
     public void setWebsite(String website) {
-        this.website = website;
+        Website = website;
     }
 }
